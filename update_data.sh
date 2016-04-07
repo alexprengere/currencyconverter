70bc0e16756af0a1e8c1c1f687b88bab473923af
Alex <alexprengere@gmail.com>
2016-04-07 10:31:33 -0400

    Move update script to the top

--- update_data.sh
+++ update_data.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 DIRNAME=`dirname $0`
-cd $DIRNAME
+cd "$DIRNAME/currency_converter"
 
 rm -f eurofxref-hist.zip eurofxref-hist.csv
 wget 'http://www.ecb.int/stats/eurofxref/eurofxref-hist.zip'
 
 unzip eurofxref-hist.zip
 rm -f eurofxref-hist.zip
-
