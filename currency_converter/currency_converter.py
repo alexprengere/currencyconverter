0e44ba6271f365a82a8ed47d84b8941c55b56351
Alex <alexprengere@gmail.com>
2016-05-03 05:24:01 -0400

    Fix doctest

--- currency_converter/currency_converter.py
+++ currency_converter/currency_converter.py
@@ -278,8 +278,8 @@ class S3CurrencyConverter(CurrencyConverter):
 def grouper(n, iterable, padvalue=None):
     """Grouper.
 
-    >>> grouper(3, 'abcdefg', 'x')
-    ('a','b','c'), ('d','e','f'), ('g','x','x')
+    >>> list(grouper(3, 'abcdefg', 'x'))
+    [('a', 'b', 'c'), ('d', 'e', 'f'), ('g', 'x', 'x')]
     """
     return zip_longest(*[iter(iterable)] * n, fillvalue=padvalue)
 
